bbf4db3e0eac125e95964ed327b71bc933b8829f
Gonzalo Peraza <ggperaza@gmail.com>
2015-10-07 11:39:49 -0500

    Cleanup 3disk-sweep.py

--- python-scripts/3disk-sweep.py
+++ python-scripts/3disk-sweep.py
@@ -1,8 +1,5 @@
-import numpy as np
 import os
 import subprocess
-import csv
-#import sys
 import itertools
 import multiprocessing
 
@@ -11,7 +8,8 @@ of the 3 disk setup. The script relies on the C program disks to run
 the simulations. The script contains some functions to analyze data
 obtained from this simulations'''
 
-def write_input_file(ac,tilt,dirname):
+
+def write_input_file(ac, tilt, dirname):
     f = open(dirname + "/input_file", "w")
     f.write("""#nParticles       1
 #seed             123456   (seed for random generator)
@@ -25,7 +23,7 @@ def write_input_file(ac,tilt,dirname):
 #timestep         1e-6     (in s, timestep for integrator)
 #relaxTime        5        (in s, time for relaxation)
 #thermalTime      5
-#runTime          5     (in s, time for simulation)
+#runTime          5        (in s, time for simulation)
 #timeForGraph     1e0      (in s, time between graphics)
 #timeForWrite     1e-6     (in s, time between writes)
 #meanR            0.02     (in m, mean disk radius)
@@ -35,56 +33,52 @@ def write_input_file(ac,tilt,dirname):
 #mu               0.1      (friction coefficient)
 #vGamma           150      (viscoelastic dissipation)
 #bCondType        1        (1: sinusoidal 2:random vib)
-"""
-)
+#wedge            1
+""")
+
 
 def run_simulation((ac, tilt)):
     reRun = False
-    #execPath = "/home/moukarzel/DEM-source/disks"
-    execPath = "/home/gperaza/Documents/RotatingDisks/DEM-source/disks"
+    execPath = "/home/moukarzel/DEM-source/disks"
     dirname = "tilt."+tilt+".ac."+ac
     if os.path.isdir(dirname):
-        if reRun:
-            pass
-        else:
+        if not reRun:
             print "Skipping ac = ", ac, "and tilt = ", tilt, \
-                  ". Already been simulated."
+                ". Already been simulated."
             return
     else:
         os.mkdir(dirname)
 
-    write_input_file(ac,tilt,dirname)
-    log = open(dirname+"/log","w")
-    errorLog = open(dirname+"/error_log","w")
+    write_input_file(ac, tilt, dirname)
+    log = open(dirname + "/log", "w")
+    errorLog = open(dirname + "/error_log", "w")
     print "running ac = ", ac, "tilt = ", tilt
-    subprocess.check_call([execPath], cwd = dirname,
-                          stdout = log, stderr = errorLog)
+    subprocess.check_call([execPath], cwd=dirname, stdout=log, stderr=errorLog)
     log.close()
     errorLog.close()
-    print "done"
+    print "done ac = ", ac, "tilt = ", tilt
 
-def main(exType):
+
+def main():
     if not os.path.isdir("3diskSimSweep"):
         os.mkdir("3diskSimSweep")
     os.chdir("3diskSimSweep")
-    #acList = ["%06.4f"%x for x in np.arange(0, 1.25, 0.005)]
+
+    # List of acceleration amplitudes
+    # acList = ["%06.4f"%x for x in np.arange(0, 1.25, 0.005)]
     acList = ["1.0000"]
-    #tiltList = ["%05.3f"%x for x in np.arange(0, 0.166, 0.001)]
-    #tiltList = ["%05.3f"%x for x in np.arange(0, 0.071, 0.001)]
+
+    # List of tilt angles.
+    # tiltList = ["%05.3f"%x for x in np.arange(0, 0.166, 0.001)]
+    # tiltList = ["%05.3f"%x for x in np.arange(0, 0.071, 0.001)]
     tiltList = ["0.000"]
-    #vibList2 = [1e-6, 3e-6, 5e-6, 8e-6, 1e-5, 3e-5, 5e-5, 8e-5, 1e-4, 1.5e-4, 2e-4]
-    #vibList = ["%09.7f"%x for x in vibList2]
-    #reRun = False
-    #if reRun:
-    #    print "reRun is set to TRUE, old simulation will be lost.Continue?[y/n]"
-    pool_size = 1
+
+    pool_size = 16
     pool = multiprocessing.Pool(processes=pool_size)
-    if exType == 1: pool_outputs = pool.map(run_simulation, itertools.product(acList, tiltList))
-    if exType == 2: pool_outputs = pool.map(run_simulation, itertools.product(vibList, tiltList))
-    pool.close() # no more tasks
+    pool.map(run_simulation, itertools.product(acList, tiltList))
+    pool.close()  # no more tasks
     pool.join()  # wrap up current tasks
     os.chdir("..")
 
 if __name__ == "__main__":
-    exType = 1 #Remember to change it in the input file
-    main(exType)
+    main()
